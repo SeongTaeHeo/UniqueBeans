b8c59721d6c15850c28a6c8c1bcf46bb74658589
gj153 <gj153@DESKTOP-A9OVHHG>
2018-01-24 09:54:59 +0900

    json 데이터 서버로 넘기는 예제

---
+++ UniqueBeansProject/src/main/java/com/uniqueBeans/biz/Beans_ProductVO.java
@@ -0,0 +1,49 @@
+package com.uniqueBeans.biz;
+
+public class Beans_ProductVO {
+	private String product_code = null;
+	private String country_name = null;
+	private String bean_name = null;
+	private String product_name = null;
+	private int product_price;
+	private String sort_code = null;
+	
+	public String getProduct_code() {
+		return product_code;
+	}
+	public void setProduct_code(String product_code) {
+		this.product_code = product_code;
+	}
+	public String getCountry_name() {
+		return country_name;
+	}
+	public void setCountry_name(String country_name) {
+		this.country_name = country_name;
+	}
+	public String getBean_name() {
+		return bean_name;
+	}
+	public void setBean_name(String bean_name) {
+		this.bean_name = bean_name;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public int getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(int product_price) {
+		this.product_price = product_price;
+	}
+	public String getSort_code() {
+		return sort_code;
+	}
+	public void setSort_code(String sort_code) {
+		this.sort_code = sort_code;
+	}
+	
+	
+}
