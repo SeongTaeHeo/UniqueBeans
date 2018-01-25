0f7b6defee5cfe5a52e47590a8766c800266a322
USER <USER@USER-PC>
2018-01-25 11:38:05 +0900

    상품입력 백엔드 작업중

---
+++ UniqueBeansProject/src/main/java/com/uniqueBeans/biz/impl/Beans_ProducServicetImpl.java
@@ -0,0 +1,23 @@
+package com.uniqueBeans.biz.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uniqueBeans.biz.Beans_ProductService;
+import com.uniqueBeans.biz.Beans_ProductVO;
+
+@Service("beans_productService")
+public class Beans_ProducServicetImpl implements Beans_ProductService {
+	
+	@Autowired
+	private Beans_ProductDAO productDAO;
+	
+	/*
+	 * 상품 정보 조회 메서드
+	 */
+	@Override
+	public void selectProductInfo(Beans_ProductVO vo){
+		System.out.println(vo.toString());
+		productDAO.selectProductInfo(vo);
+	}
+}
