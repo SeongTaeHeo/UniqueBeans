cc2a2975bcdd65e8168243aed8cc47ad72459117
SeogtaeHeo <xo1534@gmail.com>
2018-01-29 15:25:10 +0900

    json데이터 서버로 넘기기

--- UniqueBeansProject/src/main/java/com/uniqueBeans/view/Beans_ProductController.java
+++ UniqueBeansProject/src/main/java/com/uniqueBeans/view/Beans_ProductController.java
@@ -2,10 +2,12 @@ package com.uniqueBeans.view;
 
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -30,5 +32,4 @@ public class Beans_ProductController {
 		
 		return list;
 	}
-	
 }
